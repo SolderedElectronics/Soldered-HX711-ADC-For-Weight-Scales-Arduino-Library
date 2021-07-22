a4744ceb7828e46792d33794a24f2e7aaa373a69
Docs Robot <robot@example.com>
2021-07-22 12:50:59 +0000

    Committing documentation changes

--- docs/class_h_x711.js
+++ docs/class_h_x711.js
@@ -10,7 +10,7 @@ var class_h_x711 =
     [ "setScale", "class_h_x711.html#a8ce1fdccb8824c504f8c720a4bf522bc", null ],
     [ "setToZerro", "class_h_x711.html#a26d162f4692619e70b3e51e51cb7f2b4", null ],
     [ "_dout", "class_h_x711.html#aefb636db66d2af54dc073ff9c85c5bd2", null ],
-    [ "_gain", "class_h_x711.html#add2d3ec265e6faf6370e1f581dd1a8e5", null ],
+    [ "_gain", "class_h_x711.html#a9434e5b5f25fd316b762a957d91730ac", null ],
     [ "_pd_sck", "class_h_x711.html#a088424ddc7b5feba999ebd3208f47fb4", null ],
     [ "_scaleConstant", "class_h_x711.html#a65c366ac56f230f2418453d28a468d26", null ],
     [ "_zerroConstant", "class_h_x711.html#aff855a06453eab5bdfcc1cac007e7489", null ]
--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['_5fdout_51',['_dout',['../class_h_x711.html#aefb636db66d2af54dc073ff9c85c5bd2',1,'HX711']]],
-  ['_5fgain_52',['_gain',['../class_h_x711.html#add2d3ec265e6faf6370e1f581dd1a8e5',1,'HX711']]],
+  ['_5fgain_52',['_gain',['../class_h_x711.html#a9434e5b5f25fd316b762a957d91730ac',1,'HX711']]],
   ['_5fpd_5fsck_53',['_pd_sck',['../class_h_x711.html#a088424ddc7b5feba999ebd3208f47fb4',1,'HX711']]],
   ['_5fscaleconstant_54',['_scaleConstant',['../class_h_x711.html#a65c366ac56f230f2418453d28a468d26',1,'HX711']]],
   ['_5fzerroconstant_55',['_zerroConstant',['../class_h_x711.html#aff855a06453eab5bdfcc1cac007e7489',1,'HX711']]]
